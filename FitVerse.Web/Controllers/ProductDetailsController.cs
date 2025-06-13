07c8df8fdaf95ee88f37cf41844c283ae05040a5
yosra-ehab <ehabyousra842@gmail.com>
2025-06-13 20:35:45 +0300

    layout update 3

---
+++ FitVerse.Web/Controllers/ProductDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitVerse.Web.Controllers
+{
+    public class ProductDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
