65de269c7d3984714c5065f4128544e8f6766c5c
moomenabdelshaheed <moomentheone@gmail.com>
2025-06-13 15:44:06 +0300

    test

--- FitVerse.Web/Repositories/IGenericRepo.cs
+++ FitVerse.Web/Repositories/IGenericRepo.cs
@@ -6,7 +6,7 @@
         public T find(int id);
 
         public T getbyname(string name);
-        public T update(T entity);
-        public T delete(int id);
+        public void update(T entity);
+        public void delete(int id);
     }
 }
