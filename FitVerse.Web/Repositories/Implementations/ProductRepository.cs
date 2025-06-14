08bc4fbbb2838cc75f98f1f271a9732426b7d885
Ahmed Khaled <ahmedharhash0048@gmail.com>
2025-06-14 12:43:51 +0300

    made interface for repositories and made product repository

---
+++ FitVerse.Web/Repositories/Implementations/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using FitVerse.Web.Models;
+using FitVerse.Web.Repositories.Interfaces;
+
+namespace FitVerse.Web.Repositories.Implementations
+{
+    public class ProductRepository : IProduct
+    {
+        FitVerseContext db;
+        public ProductRepository(FitVerseContext _db)
+        {
+            db = _db;
+        }
+        public void Add(Product obj)
+        {
+            db.Products.Add(obj);
+        }
+
+        public void Delete(int id)
+        {
+            db.Products.Remove(GetById(id));
+        }
+
+        public void Edit(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return db.Products.ToList();
+        }
+
+        public List<Product> GetByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            return db.Products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FitVerse.Web/Repositories/Interfaces/IProduct.cs
@@ -0,0 +1,10 @@
+﻿using FitVerse.Web.Models;
+
+namespace FitVerse.Web.Repositories.Interfaces
+{
+    public interface IProduct: IRepository<Product>
+    {
+        List<Product> GetByCategory(string categoryName);
+
+    }
+}
