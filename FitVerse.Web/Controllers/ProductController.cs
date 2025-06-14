4ee4d96d97b84b260020d3db39f5d66db70d859a
Ahmed Khaled <ahmedharhash0048@gmail.com>
2025-06-14 18:27:56 +0300

    integration

--- FitVerse.Web/Controllers/ProductController.cs
+++ FitVerse.Web/Controllers/ProductController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FitVerse.Web.UnitOfWorks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FitVerse.Web.Controllers
 {
     public class ProductController : Controller
     {
+        UnitOfWork unitOfWork;
+        public ProductController(UnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
