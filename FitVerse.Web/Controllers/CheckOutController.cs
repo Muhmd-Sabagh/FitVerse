ab86a7fe54fd69b4c2b4adcedf39c3d9a9fad7b7
unknown <markmagdy822000@gmail.com>
2025-06-19 20:27:37 +0300

    fix total cost

--- FitVerse.Web/Controllers/CheckOutController.cs
+++ FitVerse.Web/Controllers/CheckOutController.cs
@@ -31,6 +31,7 @@ namespace FitVerse.Web.Controllers
         {
             List<CartItem> cartItems = _unit.CartItemRepository.GetUserCartItems();
             List<OrderItem> orderItems = _map.Map<List<OrderItem>>(cartItems);
+            var totalPricefromCartItems = _unit.CartItemRepository.getCartCost();   
 
             Order order = new Order();
             order.ShippingAddress = checkoutVM.ShippingAddress;
@@ -40,6 +41,7 @@ namespace FitVerse.Web.Controllers
             order.OrderDate = checkoutVM.OrderDate;
             order.OrderItems = orderItems;
             order.UserId = userId;
+            order.TotalAmount = totalPricefromCartItems;
             _unit.Order.Add(order);
             _unit.Save();
             Order_ViewModel orderVM = _map.Map<Order_ViewModel>(order);
