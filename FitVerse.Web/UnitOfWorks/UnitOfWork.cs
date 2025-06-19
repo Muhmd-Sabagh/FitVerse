b8ca45b19d55a7f9e6f7c7ded4c907b232661429
Ahmed Khaled <ahmedharhash0048@gmail.com>
2025-06-19 23:06:32 +0300

    removed cart item repo

--- FitVerse.Web/UnitOfWorks/UnitOfWork.cs
+++ FitVerse.Web/UnitOfWorks/UnitOfWork.cs
@@ -6,22 +6,13 @@ namespace FitVerse.Web.UnitOfWorks
 {
     public class UnitOfWork
     {
-        CartItemRepository cartItemRepository;
         ProductRepository productRepository;
         FitVerseContext _context;
         public UnitOfWork(FitVerseContext context)
         {
             _context = context;
         }
-        public CartItemRepository CartItemRepository
-        {
-            get
-            {
-                if (cartItemRepository== null)
-                    cartItemRepository = new CartItemRepository(_context);
-                return cartItemRepository;
-            }
-        }
+
         public ProductRepository ProductRepository
         {
             get
