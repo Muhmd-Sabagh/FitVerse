544b3e132ba353883dd19d80c74da5be433a7964
melsa <melsa@MUHMD-SABAGH-PC>
2025-06-19 18:53:47 +0300

    CartItem related code removed

--- FitVerse.Web/UnitOfWorks/UnitOfWork.cs
+++ FitVerse.Web/UnitOfWorks/UnitOfWork.cs
@@ -6,24 +6,8 @@ namespace FitVerse.Web.UnitOfWorks
 {
     public class UnitOfWork
     {
-        CartItemRepository cartItemRepository;
         ProductRepository productRepository;
         FitVerseContext _context;
-        public UnitOfWork(CartItemRepository cartItemRepo,FitVerseContext context, ProductRepository productRepo)
-        {
-            cartItemRepository = cartItemRepo;
-            _context = context;
-            productRepository = productRepo;
-        }
-        public CartItemRepository CartItemRepository
-        {
-            get
-            {
-                if (cartItemRepository== null)
-                    cartItemRepository = new CartItemRepository(_context);
-                return cartItemRepository;
-            }
-        }
         public ProductRepository ProductRepository
         {
             get
