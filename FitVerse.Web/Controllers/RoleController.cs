9b3d1adaf98f454305f426b73ae5fd80433e681b
Abdalla Mohamed <abdallagika2000@gmail.com>
2025-06-20 19:11:59 +0300

    latetest commit 2

---
+++ FitVerse.Web/Controllers/RoleController.cs
@@ -0,0 +1,44 @@
+﻿using FitVerse.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitVerse.Web.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+       public RoleController(RoleManager<IdentityRole> roleManager) 
+        {
+            this.roleManager = roleManager;
+        }
+        public IActionResult Create()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleViewModel roleFromReq)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole role=new IdentityRole()
+                { 
+                    Name=roleFromReq.RoleName,
+                };
+               IdentityResult result= await roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "Home");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
