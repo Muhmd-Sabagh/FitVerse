62463bb4df25eb5087fcb6e513a2c6a369269aaf
melsa <melsa@MUHMD-SABAGH-PC>
2025-06-20 18:07:22 +0300

    Add Generic Repo

---
+++ FitVerse.Web/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using FitVerse.Web.Models;
+using FitVerse.Web.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitVerse.Web.Repositories.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly FitVerseContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(FitVerseContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(int pageNumber = 1)
+        {
+            
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
