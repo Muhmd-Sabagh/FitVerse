05768f8dd07275ab57476a1b2414e661713cf785
melsa <melsa@MUHMD-SABAGH-PC>
2025-06-21 01:04:07 +0300

    V0.0

--- FitVerse.Web/Repositories/Interfaces/ICartItemRepository.cs
+++ FitVerse.Web/Repositories/Interfaces/ICartItemRepository.cs
@@ -1,9 +1,15 @@
 ﻿using FitVerse.Web.Models;
 
-
-namespace FitVerse.Web.Repositories.Interfaces
+namespace FitVerse.Web.Interfaces
 {
-    public interface ICartItemRepository : IRepository<CartItem>
+    public interface ICartItemRepository
     {
+        Task<CartItem> GetCartItemByIdAsync(int id);
+        Task<IEnumerable<CartItem>> GetCartItemsByUserIdAsync(string userId);
+        Task<CartItem> GetCartItemByProductIdAndUserIdAsync(int productId, string userId); // For updating quantity
+        Task AddCartItemAsync(CartItem cartItem);
+        Task UpdateCartItemAsync(CartItem cartItem);
+        Task DeleteCartItemAsync(int id);
+        Task DeleteCartItemsByUserIdAsync(string userId);
     }
 }
